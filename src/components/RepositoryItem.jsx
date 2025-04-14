772433ce4198fe4acc4ce71df4382acc34d7b9be
Habugabu <115887891+Habugabu@users.noreply.github.com>
2025-04-14 17:38:12 +0900

    10.3 completed

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <View>
+      <Text>
+        Full name: {item.fullName} {"\n"}
+        Description: {item.description} {"\n"}
+        Language: {item.language} {"\n"}
+        Forks: {item.forksCount} {"\n"}
+        Stars: {item.stargazersCount} {"\n"}
+        Rating: {item.ratingAverage} {"\n"}
+        Reviews: {item.reviewCount} {"\n"}
+      </Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
